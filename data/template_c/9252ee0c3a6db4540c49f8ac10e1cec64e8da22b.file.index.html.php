f3db6ed782d47737e8e29c86e63b2506c1138fbc
xkf163 <572571573@qq.com>
2014-11-03 20:07:32 +0800

    首页JS函数优化

--- data/template_c/9252ee0c3a6db4540c49f8ac10e1cec64e8da22b.file.index.html.php
+++ data/template_c/9252ee0c3a6db4540c49f8ac10e1cec64e8da22b.file.index.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.21-dev, created on 2014-11-03 13:04:05
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2014-11-03 13:07:05
          compiled from "tpl\index\index.html" */ ?>
 <?php /*%%SmartyHeaderCode:188865450762b500a30-41478977%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '9252ee0c3a6db4540c49f8ac10e1cec64e8da22b' => 
     array (
       0 => 'tpl\\index\\index.html',
-      1 => 1415016242,
+      1 => 1415016420,
       2 => 'file',
     ),
   ),
@@ -42,7 +42,6 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 	<title><?php echo $_smarty_tpl->tpl_vars['webName']->value['configvalue'];?>
 </title> 
 </head>
-
 <body>
 <div class="f_Header" style="height: 60px;width: 100%;background-color:#45729B">
 </div>
