8961f2e46398af5e2b0c09334bcb7d85c8c1e1ad
xkf163 <572571573@qq.com>
2014-11-03 19:31:30 +0800

    141103
    更多显示条件 array长度大于10

--- data/template_c/9252ee0c3a6db4540c49f8ac10e1cec64e8da22b.file.index.html.php
+++ data/template_c/9252ee0c3a6db4540c49f8ac10e1cec64e8da22b.file.index.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.21-dev, created on 2014-11-03 03:39:38
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2014-11-03 12:27:15
          compiled from "tpl\index\index.html" */ ?>
 <?php /*%%SmartyHeaderCode:188865450762b500a30-41478977%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '9252ee0c3a6db4540c49f8ac10e1cec64e8da22b' => 
     array (
       0 => 'tpl\\index\\index.html',
-      1 => 1414982376,
+      1 => 1415014032,
       2 => 'file',
     ),
   ),
@@ -76,10 +76,12 @@ $_smarty_tpl->tpl_vars['typeVal']->_loop = true;
 </a></li>
     				<?php } ?>
     			 	</ul>
+    			 	<?php if (count($_smarty_tpl->tpl_vars['typeValArray']->value)>10) {?>
                     <div class="type_more fr">
                         <span onclick="seeMore(this)" id="seeMore"><b></b>更多</span>
                         <span onclick="seeLess(this)" id="seeLess" style="display: none"><b></b>收起</span>
                     </div>
+                    <?php }?>
                 </dd>
            </dl>
             <?php } ?>
